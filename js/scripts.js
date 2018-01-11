ff73117d69ddb3f75893f335c05fdae8ae8495cd
dalia ramos and kaila goff <dalia@>
2018-01-10 16:10:18 -0800

    fix spans so page will work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("input#name").val();
-    var schedulingInput = $("input#schedulingInput").val();
     var dateInput = $("input#dateInput").val();
     var startInput = $("input#start").val();
     var endInput = $("input#end").val();
 
 
     $(".name").text(nameInput);
-    $(".scheduling").text(schedulingInput);
     $(".date").text(dateInput);
     $(".start").text(startInput);
     $(".end").text(endInput);
